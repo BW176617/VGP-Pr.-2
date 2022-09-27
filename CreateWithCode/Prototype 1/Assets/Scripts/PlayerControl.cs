79c58821f441e13e15081dc56c3637a8508b047b
BW176617 <176617@student.vansd.org>
2022-09-27 09:14:04 -0700

    Clarified Code

--- CreateWithCode/Prototype 1/Assets/Scripts/PlayerControl.cs
+++ CreateWithCode/Prototype 1/Assets/Scripts/PlayerControl.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    //change
-    public float speed = 20f;
-    public float turnSpeed;
-    public float horizontalInput;
-    public float forwardInput;
+    //Private Variables
+    private float speed = 20.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
  
     // Start is called before the first frame update
     void Start()
@@ -19,11 +19,13 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //This is where we get player input
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
       
         // Move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //Rotates the car based on horizontal input
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
