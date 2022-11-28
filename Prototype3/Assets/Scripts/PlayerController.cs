8ead1ef14a070b9cab19bcab271ee3de491e44c5
BW176617 <176617@student.vansd.org>
2022-11-28 09:07:54 -0800

    .

--- Prototype3/Assets/Scripts/PlayerController.cs
+++ Prototype3/Assets/Scripts/PlayerController.cs
@@ -5,20 +5,32 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool IsOnGround = true;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        Physic.gravity *= gravityModifier;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && IsOnGround)
         {
-            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            IsOnGround = false;
         }
+       
     }
+       
+    Private void OnCollisionEnter(Collision collision)
+        {
+            IsOnGround = true;
+        }
+    
 }
